ec065b24bc0a7c011e1331a8707707ea5af87c86
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-07-08 22:28:14 +0000

    adding show logic for url username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
 
-  def show
+  # We need the find_by method, because the value returned by params.fetch(:username) in our example is "alice", which won’t work with find, because that method expects an ID number.
+#   We want a 400 error, which are errors caused by normal operation, like someone trying to navigate to a location that doesn’t exist.
 
+# There’s actually a variation of find_by called find_by! with an exclamation mark. The purpose of that is to throw the record not found error:
+
+
+  def show
+    @user = User.find_by!(username: params.fetch(:username))
   end
 
 end
