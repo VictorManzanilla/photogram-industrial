8c3e6476839b2cb06ab886300774bad89390008c
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-07-08 22:19:56 +0000

    url username route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root "photos#index"
+
+  devise_for :users
+
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  devise_for :users
+  get "/:username" => "users#show", as: :user
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
