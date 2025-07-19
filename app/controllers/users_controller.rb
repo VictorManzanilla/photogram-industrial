b2c2ace6f3936d94731305ea0286553db8cb7bc4
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-07-19 21:42:30 +0000

    feed photos rendering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,8 @@ class UsersController < ApplicationController
     @user = User.find_by!(username: params.fetch(:username))
   end
 
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
 end
